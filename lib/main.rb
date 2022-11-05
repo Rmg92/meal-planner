4ade42496bf93fd4e1843e345c630b5ad500304d
Ricardo <86142611+Rmg92@users.noreply.github.com>
2022-11-05 08:07:41 +0100

    Choose 4 random recipes from recipe list

--- lib/main.rb
+++ lib/main.rb
@@ -6,7 +6,4 @@ recipes.readlines(chomp: true).each { |recipe| recipe_list << recipe }
 puts "Sunday: #{recipe_list[rand(0..(recipe_list.size - 1))]}"
 puts "Monday: #{recipe_list[rand(0..(recipe_list.size - 1))]}"
 puts "Tuesday: #{recipe_list[rand(0..(recipe_list.size - 1))]}"
-puts 'Wednesday: Soup or leftovers'
 puts "Thursday: #{recipe_list[rand(0..(recipe_list.size - 1))]}"
-puts 'Friday: Meat'
-puts 'Saturday: Meat'
